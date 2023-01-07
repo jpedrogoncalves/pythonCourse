1393c289a43da851ea4bdf6e4ae0c4c5b985798d
João Pedro <alencargoncalvesj@gmail.com>
2023-01-07 15:48:06 -0300

    introdução as listas

---
+++ aula44.py
@@ -0,0 +1,13 @@
+"""
+listas em python
+tipo list = mutável
+suporta vários valores de qualquer tipo
+conhecimentos reutilizaveis - indices e fatiamento
+métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+string = 'ABCDE'
+lista = [123, True, 'João Pedro', 1.2, [123, 456]]
+# lista = list() forma não comum
+print(lista)
+lista[3] = 'Joanaaasdoisajdois'
+print(lista)
