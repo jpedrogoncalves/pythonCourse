074ed4bea3bee642655803c3f6cd2c68df5b9763
João Pedro <alencargoncalvesj@gmail.com>
2023-01-07 17:17:20 -0300

    exercicio com numeração dinamica de indices

---
+++ aula48.py
@@ -0,0 +1,8 @@
+"""
+exercício: exibir os indices da lista
+"""
+lista = ['Luiz', 'João', 'Camila', 'Laura', 'Rinaldo', 'Ferreira', 'Pit']
+count = 0
+for nome in lista:
+    print(count, nome)
+    count += 1
