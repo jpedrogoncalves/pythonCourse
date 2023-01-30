c68a61fe7a65ea555da663ed4545ed6cb9c62223
João Pedro <alencargoncalvesj@gmail.com>
2023-01-30 20:24:11 -0300

    aula 84: introdução às list comprehension

---
+++ aula84.py
@@ -0,0 +1,8 @@
+# List Comprehension em python
+# é uma forma rápida para criar listas a partir de iteráveis
+lista = []
+# for numero in range(10):
+#     lista.append(numero)
+
+lista = [x * 2 for x in range(10)]
+print(lista)
