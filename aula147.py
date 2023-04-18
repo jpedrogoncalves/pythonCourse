38010db433ab0a93128f8a54219072e5d8ad739d
João Pedro <alencargoncalvesj@gmail.com>
2023-04-17 21:38:18 -0300

    aula 147, pt2: exemplo de dunder methods

--- aula147.py
+++ aula147.py
@@ -16,25 +16,40 @@
 # __str__(self) - str
 # __repr__(self) - str
 class Ponto:
-    def __init__(self, x, y, z='String') -> None:
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.z = z
 
-    def __str__(self):
-        return f'({self.x}, {self.y})'
+    # def __str__(self):
+    #     return f'({self.x}, {self.y})'
 
     def __repr__(self):
         class_name = type(self).__name__
         # class_name = self.__class__.__name__ => mesma coisa
 
         # representação do objeto
-        return f'{class_name}(x={self.x!r}, y={self.y!r}, z={self.z!r})'
+        return f'{class_name}(x={self.x!r}, y={self.y!r})'
+    
+    def __add__(self, other):
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        return Ponto(new_x, new_y)
 
-p1 = Ponto(1, 2)
-p2 = Ponto(897, 298)
+    def __gt__(self, other):
+        resultado_self = self.x + self.x
+        resultado_other = other.x + other.y
+        return resultado_self > resultado_other
 
 # * maneiras de chamar o repr
-print(repr(p1))
-print(f'{p1!r}')
-print(p2)
+# print(repr(p1))
+# print(f'{p1!r}')
+# print(p2)
+
+if __name__ == '__main__':
+    p1 = Ponto(4, 2) # 6
+    p2 = Ponto(6, 4) # 10
+    p3 = p1 + p2
+    print(p3)
+    print('P1 é maior que P2', p1 > p2)
+    print('P2 é maior que P1', p2 > p1)
+
